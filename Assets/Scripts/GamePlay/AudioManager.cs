fc2bc17452476158ebc3b4f88ab56252b76b196b
Binh Nguyen <thecolors.notification@gmail.com>
2026-04-26 20:13:27 +0700

    update

--- Assets/Scripts/GamePlay/AudioManager.cs
+++ Assets/Scripts/GamePlay/AudioManager.cs
@@ -37,7 +37,7 @@ public class AudioManager : Singleton<AudioManager>
 			SetBGEnabled(userData.musicOn);
 			SetSFXEnabled(userData.soundOn);
 		}
-		PlayBG(BGType.Default);
+	//	PlayBG(BGType.Default);
 	}
 
 	public float GetVolumeBG() => _bgAudioSource.volume;
--- Assets/Scripts/GamePlay/GameManagerInGame.cs
+++ Assets/Scripts/GamePlay/GameManagerInGame.cs
@@ -69,12 +69,12 @@ public class GameManagerInGame : Singleton<GameManagerInGame>
         userData = Game.Data.Load<UserData>();
         if (userData != null && !userData.isDefaultData)
         {
-            userData.SetDefaultData();
-            userData.Save();
+         //   userData.SetDefaultData();
+        //    userData.Save();
         }
         if (!InitLevel)
         {
-            LoadData();
+        //    LoadData();
         }
         API.Initialize();
     }
